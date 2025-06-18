9140bbb2295091d8758251b83cb7f1f8f0ac3eda
eunchan96 <taegwon02@gmail.com>
2025-06-18 15:41:53 +0900

    PersonRepository version

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,7 +1,17 @@
 package com.back;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public PersonRepository personRepository() {
+        return new PersonRepository(1);
+    }
+
+    @Bean
+    public PersonRepository personRepositoryV2() {
+        return new PersonRepository(2);
+    }
 }
