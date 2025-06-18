20054ed561c610324e7aa478ab4109e5bdbfd0a4
eunchan96 <taegwon02@gmail.com>
2025-06-18 15:08:22 +0900

    AppConfig

---
+++ src/main/java/com/back/AppConfig.java
@@ -0,0 +1,12 @@
+package com.back;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public PersonService personService() {
+        return new PersonService();
+    }
+}
