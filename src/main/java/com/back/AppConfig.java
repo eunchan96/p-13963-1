c1bea301ea906a1ad233544deb3a58cbc30444de
eunchan96 <taegwon02@gmail.com>
2025-06-19 11:20:47 +0900

    ApplicationRunner 익명클래스

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +9,23 @@ public class AppConfig {
     @Bean
     int version() {
         return 2;
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            System.out.println("AppConfig.applicationRunner() 작동");
+
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1() 작동");
+    }
+
+    private void work2() {
+        System.out.println("work2() 작동");
     }
 }
