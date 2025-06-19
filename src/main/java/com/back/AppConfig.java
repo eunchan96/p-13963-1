75cecce3c3b04354352b957454596e3451f9b88a
eunchan96 <taegwon02@gmail.com>
2025-06-19 10:47:09 +0900

    ApplicationRunner

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +9,10 @@ public class AppConfig {
     @Bean
     int version() {
         return 2;
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner() {
+        return new MyApplicationRunner();
     }
 }
