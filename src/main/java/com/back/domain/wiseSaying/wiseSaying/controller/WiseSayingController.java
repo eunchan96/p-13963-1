48ef04929d039beacf703c8bf0b60a4cc1d5470d
eunchan96 <taegwon02@gmail.com>
2025-06-20 14:34:50 +0900

    Transactional, auto-commit

--- src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -5,6 +5,7 @@ import com.back.domain.wiseSaying.wiseSaying.service.WiseSayingService;
 import com.back.standard.util.service.MarkdownService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,6 +45,7 @@ public class WiseSayingController {
 
     @GetMapping("/wiseSayings/{id}/delete")
     @ResponseBody
+    @Transactional
     public String delete(@PathVariable int id) {
         WiseSaying wiseSaying = wiseSayingService.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 ID의 명언이 존재하지 않습니다."));
 
